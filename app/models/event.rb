7e04ba7cf986c4545071c3cb9b8e9d50a1a4a801
Chris Williams <chrilliams@gmail.com>
2014-08-18 22:43:15 +0100

    display dates on graph

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ActiveRecord::Base
   scope :get_business_service_events, ->(bus_id) { BusinessSystem.find(bus_id).events }
   scope :all_during_year_and_month, ->(date) { where("strftime('%Y/%m', start_time) = ?", date)}
   scope :all_between_dates, ->(start_date,end_date) { where("start_time between ? AND ?", start_date, end_date)}
-  scope :hourlybreakdown, ->{ group("strftime( '%Y%m%d%H', start_time)").count}
+  scope :hourlybreakdown, ->{ group("strftime( '%Y-%m-%dT%H:00', start_time)").count}
 
   def self.unique_dates
     Event.order("start_time desc").pluck( "strftime( '%Y/%m', start_time )" ).uniq
